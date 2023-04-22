7debd66fb1bbb4ad553109cd9084f66a4e787eed
Sven Svensson <sven@bringsrud.net>
2023-04-22 06:10:44 +0200

    Initial Commit

---
+++ CurrencyInsight/CurrencyInsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyInsightApp.swift
+//  CurrencyInsight
+//
+//  Created by Sven Svensson on 22.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyInsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
